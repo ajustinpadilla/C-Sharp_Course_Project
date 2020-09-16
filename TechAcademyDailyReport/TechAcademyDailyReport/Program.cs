b686e81c19d23ede4d4d770f268d0382898dcba9
Angel Padilla <Ajustinpadilla@gmail.com>
2020-09-16 15:17:37 -0600

    update int values and lowercase all data types

--- TechAcademyDailyReport/TechAcademyDailyReport/Program.cs
+++ TechAcademyDailyReport/TechAcademyDailyReport/Program.cs
@@ -13,19 +13,19 @@ namespace TechAcademyDailyReport
             Console.WriteLine("The Tech Academy");
             Console.WriteLine("Student Daily Report.");
             Console.WriteLine("What is your name?");
-            String Name = Console.ReadLine();
+            string Name = Console.ReadLine();
             Console.WriteLine("What course are you on?");
-            String CourseOn = Console.ReadLine();
+            string CourseOn = Console.ReadLine();
             Console.WriteLine("What page number?");
-            String Page = Console.ReadLine();
+            int Page = Console.ReadLine();
             Console.WriteLine("Do you need help with anything? Please answer \"true\" or \"false.\"");
             bool NeedHelp = Convert.ToBoolean(Console.ReadLine());
             Console.WriteLine("Were there any positive experiences you’d like to share? Please give specifics.");
-            String SharedExperience = Console.ReadLine();
+            string SharedExperience = Console.ReadLine();
             Console.WriteLine("Is there any other feedback you’d like to provide? Please be specific.");
-            String Feedback = Console.ReadLine();
+            string Feedback = Console.ReadLine();
             Console.WriteLine("How many hours did you study today?");
-            String StudyHours = Console.ReadLine();
+            int StudyHours = Console.ReadLine();
             Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly. Have a great day!");
             Console.Read();
         }
