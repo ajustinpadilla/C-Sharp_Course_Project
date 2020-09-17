974654f5871b6189c1ee777ab6c48af6fa77382c
Angel Padilla <Ajustinpadilla@gmail.com>
2020-09-17 00:14:52 -0600

    String methods

---
+++ Strings/Strings/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+
+
+namespace Strings
+{
+    class Program
+    {
+        static void Main()
+        {
+            string fName = "Angel";
+            string mName = "Justin";
+            string lName = "Padilla";
+            string fullName = fName + " " + mName + " " + lName;
+
+            Console.WriteLine(fullName);
+            Console.WriteLine(fullName.ToUpper());
+            Console.ReadLine();
+
+            StringBuilder myparagraph = new StringBuilder();
+
+            bool RunThis = true;
+            int num = 1;
+
+            while (RunThis)
+            {
+                myparagraph.Append("This is sentence " + num + ". ");
+                num++;
+                Console.WriteLine(myparagraph);
+                Console.ReadLine();
+                RunThis = myparagraph.Length <= 50;
+            }
+        }
+    }
+}
