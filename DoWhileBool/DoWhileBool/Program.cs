4aa11461a61007b2319dd743b790cd1df8d0fb7a
Angel Padilla <Ajustinpadilla@gmail.com>
2020-09-16 23:40:36 -0600

    While/Do-While comparison

---
+++ DoWhileBool/DoWhileBool/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoWhileBool
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("How many people do I have in my family");
+            int NumGuess = Convert.ToInt32(Console.ReadLine());
+            bool keepGoing = NumGuess == 6;
+
+
+            do
+            {
+                switch (NumGuess)
+                {
+                    case 6:
+                        Console.WriteLine("You guessed correctly! I do have 6 people in my family");
+                        keepGoing = true;
+                        break;
+                    default:
+                        Console.WriteLine("You guessed " + NumGuess + ". That is incorrect! Please try again.");
+                        Console.WriteLine("How many people do I have in my family");
+                        NumGuess = Convert.ToInt32(Console.ReadLine());
+                        break;
+                }
+            }
+
+            while (!keepGoing);
+            Console.Read();
+        }
+    }
+}
