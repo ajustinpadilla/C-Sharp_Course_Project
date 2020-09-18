d5c34e4d712f65728724ce9feca4c11e625ad10f
Angel Padilla <Ajustinpadilla@gmail.com>
2020-09-18 12:12:21 -0600

    Create a class assignment pg 216

---
+++ ClassAssignment/ClassAssignment/Math.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAssignment
+{
+    public class Math
+    {
+        public static int AddThree(int num) { return num + 3; }
+
+        public static int MultiThree(int num) { return num * 3; }
+
+        public static int SubThree(int num) { return num - 3; }
+    }
+}
---
+++ ClassAssignment/ClassAssignment/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please choose a number:\n>>>");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int added = Math.AddThree(num);
+            int multi = Math.MultiThree(num);
+            int sub = Math.SubThree(num);
+
+            Console.WriteLine(num + " plus three equals:\n" + added);
+            Console.WriteLine(num + " multiplied three equals:\n" + multi);
+            Console.WriteLine(num + " subtracted three equals:\n" + sub);
+            Console.ReadLine();
+        }
+    }
+}
