03e1778c048cdb8a5bf4be82bbc95c0ac0e915c9
Angel Padilla <Ajustinpadilla@gmail.com>
2020-09-18 14:39:29 -0600

    Specify parameters Assignment pg 221

---
+++ SpecifyParameters/SpecifyParameters/Math.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpecifyParameters
+{
+    public class Math
+    {
+        public void OptionalParam(int num1, int num2)
+        {
+            int sum = num1 + 17;
+            Console.WriteLine(num2);
+        }
+    }
+}
---
+++ SpecifyParameters/SpecifyParameters/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpecifyParameters
+{
+    class Program
+    {
+        static void Main()
+        {
+            Math nums = new Math();
+
+            nums.OptionalParam(2, 17);
+            nums.OptionalParam(num1: 71, num2: 25);
+            Console.ReadLine();
+        }
+    }
+}
