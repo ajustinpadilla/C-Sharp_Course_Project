0af9ea63b975b436c517c582e8381c5b34914fd8
Angel Padilla <Ajustinpadilla@gmail.com>
2020-09-18 13:11:14 -0600

    Method overloading Assignment pg 219

---
+++ MethodOverloading/MethodOverloading/Math.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverloading
+{
+    public class Math
+    {
+        public static int Exec(int num) { return num + 20; }
+
+        public static decimal Exec(decimal num) { return num * 20; }
+
+        public static int Exec(string num)
+        {
+            int intNum = Convert.ToInt32(num);
+            int Num = intNum * 7;
+            return Num;
+           
+        }
+    }
+}
---
+++ MethodOverloading/MethodOverloading/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverloading
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Input a number:\n>>>");
+            int Num = Convert.ToInt32(Console.ReadLine());
+
+            Num = Math.Exec(Num);
+            Console.WriteLine("Your new number:\n" + Num);
+
+            Console.Write("\nInput another number:\n>>>");
+            Decimal Num1 = Convert.ToDecimal(Console.ReadLine());
+
+            Num1 = Math.Exec(Num1);
+            Console.WriteLine("Your second new number:\n" + Num1);
+
+            Console.Write("\nInput another number:\n>>>");
+            string Num2 = Console.ReadLine();
+
+            int Num3 = Math.Exec(Num2);
+            Console.WriteLine("Your third new number:\n" + Num3);
+
+
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
