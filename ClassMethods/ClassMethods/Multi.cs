ebfff76b2cd06a815326f873acbf6c91536ff3f0
Angel Padilla <Ajustinpadilla@gmail.com>
2020-09-18 20:19:35 -0600

    Inheritance Assignment pg 229

--- ClassMethods/ClassMethods/Multi.cs
+++ ClassMethods/ClassMethods/Multi.cs
@@ -12,5 +12,9 @@ namespace ClassMethods
         {
             num2 = num1 / 2;
         }
+
+        public int DubTogether(int num1, int num2) { return (num1 + num2)*2; }
+        public decimal DubTogether(decimal num1, decimal num2) { return (num1 + num2) * 2; }
+        public static string Concat(string word1, string word2) { return word1 + " " + word2; }
     }
 }
