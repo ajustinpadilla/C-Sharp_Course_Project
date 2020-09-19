7e8bb8fb8531dd422d91b18a4ed5a32fa3e11256
Angel Padilla <Ajustinpadilla@gmail.com>
2020-09-18 19:43:35 -0600

    Class methods Assignment pg 224

---
+++ ClassMethods/ClassMethods/Multi.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethods
+{
+    public class Multi
+    {
+        public void Half(int num1, out int num2)
+        {
+            num2 = num1 / 2;
+        }
+    }
+}
---
+++ ClassMethods/ClassMethods/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethods
+{
+    class Program
+    {
+        static void Main()
+        {
+            Multi output = new Multi();
+
+            Console.Write("Please input a number:\n>>>");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            output.Half(num1, out int num2);
+            Console.WriteLine("Your number was halfed: " + num2);
+            Console.ReadLine();
+        }
+    }
+}
