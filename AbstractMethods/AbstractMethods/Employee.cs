bac4de0f1cc53455c82546e5cbc90824028abdba
Angel Padilla <Ajustinpadilla@gmail.com>
2020-09-19 02:51:56 -0600

    Overloading operators Assignment pg 245

--- AbstractMethods/AbstractMethods/Employee.cs
+++ AbstractMethods/AbstractMethods/Employee.cs
@@ -8,6 +8,7 @@ namespace AbstractMethods
 {
      public class Employee : Person, IQuittable
     {
+        public int Id { get; set; }
         public override void SayName()
         {
             base.SayName();
@@ -16,6 +17,17 @@ namespace AbstractMethods
         public void Quit()
         {
             Console.WriteLine("I quit!");
+        }
+
+        public static bool operator==(Employee employee1, Employee employee2)
+        {
+            bool equal = employee1.Id == employee2.Id;
+            return equal;
+        }
+        public static bool operator!=(Employee employee1, Employee employee2)
+        {
+            bool notEqual = employee1.Id != employee2.Id;
+            return notEqual;
         }
     }
 }
