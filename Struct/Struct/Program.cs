565d408585ffd7527eae4da709fde6c56b07789f
Angel Padilla <Ajustinpadilla@gmail.com>
2020-09-19 14:55:02 -0600

    Struct Assignment pg 260

---
+++ Struct/Struct/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number num = new Number() { Amount = 17};
+            Console.WriteLine(num.Amount);
+            Console.Read();
+            
+
+        }
+    }
+}
