5fac5ec8f5e26149699e0f989aff81a084f0341a
Angel Padilla <Ajustinpadilla@gmail.com>
2020-09-19 22:02:47 -0600

    Write to a file assignment pg 290

---
+++ TwentyOne/TwentyOne/TwentyOneDealer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwentyOne
+{
+    public class TwentyOneDealer : Dealer
+    {
+        private List<Card> _Hand = new List<Card>();
+        public List<Card> Hand { get { return _Hand; } set { _Hand = value; } }
+        public bool Stay { get; set; }
+        public bool isBusted { get; set; }
+        
+    }
+}
---
+++ WriteToAFile/WriteToAFile/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace WriteToAFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Give me a number.\n>>>");
+            string num = Console.ReadLine();
+            File.WriteAllText(@"C:\C-Shrap_Course\logs\WrittenFile.txt", "your number: " + num);
+            Console.WriteLine(File.ReadAllText(@"C:\C-Shrap_Course\logs\WrittenFile.txt"));
+            Console.ReadLine();
+        }
+    }
+}
