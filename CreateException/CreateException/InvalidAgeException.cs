5ca4c8938d066c4e297d0e37f5a24725c381fe7a
Angel Padilla <Ajustinpadilla@gmail.com>
2020-09-20 01:55:17 -0600

    Create an Exception pg 307

---
+++ CreateException/CreateException/InvalidAgeException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateException
+{
+    public class InvalidAgeException : Exception
+    {
+        public InvalidAgeException()
+            : base() { }
+        public InvalidAgeException(string message)
+            : base(message) { }
+    }
+}
---
+++ CreateException/CreateException/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool validAnswer = false;
+            int age = 0;
+            try
+            {
+                while (!validAnswer)
+                {
+                    Console.WriteLine("What's your age?");
+                    validAnswer = int.TryParse(Console.ReadLine(), out age);
+                    if (!validAnswer) Console.WriteLine("Please enter digits only, no decimals.");
+                }
+                if (age < 1)
+                {
+                    throw new InvalidAgeException();
+                }
+
+            }
+            catch (InvalidAgeException)
+            {
+                Console.WriteLine("That is not a valid age. Please give an age above 0.");
+                Console.ReadLine();
+                return;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Something went wrong. Please try again later");
+                Console.ReadLine();
+                return;
+            }
+
+            Console.WriteLine("You were born in {0}",DateTime.Now.Year - age);
+            Console.ReadLine();
+        }
+    }
+}
